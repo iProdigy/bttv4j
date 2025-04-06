77546f24b335de7be3870b655f92e42771b0af95
iProdigy <gitprodigy@proton.me>
2025-04-06 00:11:57 -0500

    chore(websocket): release more memory on close

--- websocket/src/main/java/io/github/iprodigy/bttv/ws/domain/EmoteCreateEvent.java
+++ websocket/src/main/java/io/github/iprodigy/bttv/ws/domain/EmoteCreateEvent.java
@@ -3,6 +3,8 @@ package io.github.iprodigy.bttv.ws.domain;
 import io.github.iprodigy.bttv.common.Emote;
 
 /**
+ * Create events are sent when a new emote is uploaded to the subscribed channel, or when a new shared emote is added.
+ *
  * @param channel The emote that was created.
  * @param emote   The channel that the emote was created in.
  */
